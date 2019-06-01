568a5e88b2fba3e801c7e316f36b24373434567a
Lucas Barrozo <lucaslbarrozo@gmail.com>
2019-06-01 16:02:11 -0300

    cria o vetor de contador de operacoes

--- java1/TP3/src/Log.java
+++ java1/TP3/src/Log.java
@@ -1,2 +1,56 @@
+import javax.swing.*;
+
 public class Log {
+
+    public Log(){
+
+    }
+
+    public Log (int operacao, double n1, double n2,int contador, String[] log) {
+        adicionaOperacao(operacao, n1, n2, contador, log);
+    }
+
+    public static void adicionaOperacao(int operacao, double n1, double n2,int contador, String[] log){
+        final int ULTIMAPOS = 4;
+        Calculadora calc = new Calculadora();
+        String contaMemorizar = "";
+
+
+        switch (operacao) {
+            case 1:
+                contaMemorizar = n1 + " + " + n2 + " = " + calc.soma(n1,n2);
+                break;
+            case 2:
+                contaMemorizar = n1 + " - " + n2 + " = " + calc.subtrai(n1,n2);
+                break;
+            case 3:
+                contaMemorizar = n1 + " * " + n2 + " = " + calc.multiplica(n1,n2);
+                break;
+            case 4:
+                contaMemorizar = n1 + " / " + n2 + " = " + calc.divide(n1,n2);
+                break;
+            default:
+                break;
+        }
+
+        if (!contaMemorizar.isEmpty()) {
+            if (contador < 5) {
+                log[contador] = contaMemorizar;
+            } else {
+                for (int i = 0; i < ULTIMAPOS; i++) {
+                    log[i] = log[i+1];
+                }
+                log[ULTIMAPOS] = contaMemorizar;
+            }
+        }
+    }
+
+    public void contaOperacao(int operacao,int contSoma, int contSub, int contMult, int contDiv, int[] contOperacoes) {
+
+    }
+
+    public String toString(String[] log) {
+
+        return log[0] +"\n" + log[1]+"\n" + log[2]+"\n" + log[3]+"\n" + log[4];
+    }
 }
--- java1/TP3/src/Main.java
+++ java1/TP3/src/Main.java
@@ -1,43 +1,64 @@
 import javax.swing.*;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         final int FIM = 0;
-        int opcao;
+        final int OPERACOES = 4;
+        final int TAM = 5;
+        int opcao,contador = 0, contSoma = 0, contSub = 0, contMult = 0, contDiv = 0;
         double n1,n2;
-        Calculadora calc;
 
+        Calculadora calc;
+        String[] log = new String[TAM];
+        Log logMemoria;
         opcao = menu();
+
         while (opcao != FIM) {
 
-            n1 = leNumero("Digite qual o valor de n1: ");
-            n2 = leNumero("Digite qual o valor de n2: ");
             calc = new Calculadora();
+            logMemoria = new Log();
 
 
             switch (opcao){
                 case 1:
+                    n1 = leNumero("Digite qual o valor de n1: ");
+                    n2 = leNumero("Digite qual o valor de n2: ");
                     JOptionPane.showMessageDialog(null, calc.toString() +"\nA soma de "+n1+" e "+n2+" = "+ calc.soma(n1,n2));
+                    logMemoria.adicionaOperacao(opcao,n1,n2,contador,log);
                     break;
                 case 2:
+                    n1 = leNumero("Digite qual o valor de n1: ");
+                    n2 = leNumero("Digite qual o valor de n2: ");
                     JOptionPane.showMessageDialog(null, calc.toString() +"\nA subtração de "+n1+" e "+n2+" = "+ calc.subtrai(n1,n2));
+                    logMemoria.adicionaOperacao(opcao,n1,n2,contador,log);
                     break;
                 case 3:
+                    n1 = leNumero("Digite qual o valor de n1: ");
+                    n2 = leNumero("Digite qual o valor de n2: ");
                     JOptionPane.showMessageDialog(null, calc.toString() +"\nA multiplicação de "+n1+" e "+n2+" = "+ calc.multiplica(n1,n2) );
+                    logMemoria.adicionaOperacao(opcao,n1,n2,contador,log);
                     break;
                 case 4:
+                    n1 = leNumero("Digite qual o valor de n1: ");
+                    n2 = leNumero("Digite qual o valor de n2: ");
                     if (n2 == 0){
                         JOptionPane.showMessageDialog(null, calc.toString() +"\nO valor de n2 é "+n2+". Não pode dividir por 0");
+                        logMemoria.adicionaOperacao(opcao,n1,n2,contador,log);
                         break;
                     }
                     JOptionPane.showMessageDialog(null, calc.toString() +"\nA divisão de "+n1+" e "+n2+" = "+ calc.divide(n1,n2));
+                    logMemoria.adicionaOperacao(opcao,n1,n2,contador,log);
                     break;
 
-                    default:
-                        JOptionPane.showMessageDialog(null, "Selecione uma opção válida");
+                case 5:
+                    JOptionPane.showMessageDialog(null, logMemoria.toString(log));
+                    break;
+
+                default:
+                    JOptionPane.showMessageDialog(null, "Selecione uma opção válida");
                         break;
             }
+            contador++;
             opcao = menu();
         }
     }
@@ -49,16 +70,14 @@ public class Main {
         if (input == null || input.isEmpty()){
             input = "0";
         }
-
         opcao = Double.parseDouble(input);
-
         return opcao;
     }
 
     public static int menu(){
         String input;
         int opcao;
-        input = JOptionPane.showInputDialog(null, "Digite:\n1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n0 - Sair");
+        input = JOptionPane.showInputDialog(null, "Digite:\n1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n5 - Log\n0 - Sair");
         opcao = Integer.parseInt(input);
 
         return opcao;
